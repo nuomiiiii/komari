54fbf580bf18576b2d0346db8b17c70257b9aa61
Nomi <zhout5217@163.com>
2026-08-08 19:09:06 +0000

    fix: embed updated system UI and simplify theme fallback

--- web/public/systemUI/dist/assets/chunk-_layout-CEp2FJPY.js
+++
@@ -1,1 +0,0 @@
-import{j as t,p as e,O as s}from"./entry-index-uOlnfgz6.js";function n(){return t.jsx(e,{direction:"column",gap:"4",className:"p-0 md:p-4",children:t.jsx(s,{})})}export{n as default};
---
+++ web/public/systemUI/dist/assets/chunk-internal-DLXqjZ6O.js
@@ -0,0 +1,1 @@
+import{aD as e,a5 as t}from"./entry-index-BZxkmtl8.js";var i=t;i.dispatchDiscreteCustomEvent=e;i.Root=t;
